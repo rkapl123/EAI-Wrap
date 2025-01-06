805a55788176c325d047e9fb255229cf79a39e4f
rkapl123 <rolandk@kapl.org>
2025-01-06 20:36:26 +0100

    easier version setting, also current year into LICENSE

--- fetchParamDescriptionsUpdateVersion.pl
+++ fetchParamDescriptionsUpdateVersion.pl
@@ -1,8 +1,12 @@
 use strict; use feature 'unicode_strings'; use Time::localtime;
 # enter new version to update all modules with
-print "new version (empty to skip and only fetch param descriptions into Wrap.pm):";
-my $newVersion = <STDIN>;
-chomp $newVersion;
+my $data = read_file ("Makefile.PL");
+my ($newVersion) = ($data =~ /', version => 1\.([\d]+)},/);
+$newVersion++; $newVersion="1.$newVersion";
+print "new version is $newVersion (any entry to accept, empty to skip and only fetch param descriptions into Wrap.pm):";
+my $doNewVersion = <STDIN>;
+chomp $doNewVersion;
+
 # used for CONFIGURATION REFERENCE, fetches comments from %hashCheck in Common.pm and translates them to pod =items
 open (COMMONFILE, "<".'lib\EAI\Common.pm') or die ('can\'t open lib\EAI\Common.pm for reading');
 my $insertIntoEAIWrap; my $return; my $startParsing;
@@ -32,27 +36,31 @@ while (<COMMONFILE>){
 }
 close COMMONFILE;
 
-my $data = read_file ("Makefile.PL");
-print "updating version for Makefile.PL\n";
-$data =~ s/', version => [\d\.]+},/', version => $newVersion},/g if $newVersion;
+print "updating version to $newVersion for Makefile.PL\n";
+$data =~ s/', version => [\d\.]+},/', version => $newVersion},/g if $doNewVersion;
 write_file("Makefile.PL", $data);
 
 my $curYear = localtime->year()+1900;
-my $data = read_file ("lib/EAI/Wrap.pm");
-print "updating version and API descriptions for Wrap.pm\n";
+$data = read_file ("lib/EAI/Wrap.pm");
+print "updating ".($doNewVersion ? "version to $newVersion/year of copyright" : "copyright")." and API descriptions for Wrap.pm\n";
 $data =~ s/^(.*?)=head2 CONFIGURATION REFERENCE\n\n(.*?)=head1 COPYRIGHT(.*?)$/$1=head2 CONFIGURATION REFERENCE\n\n${insertIntoEAIWrap}=head1 COPYRIGHT$3/s;
-$data =~ s/^package EAI::(.*?) (.*?);\n(.*?)/package EAI::$1 $newVersion;\n$3/s if $newVersion;
+$data =~ s/^package EAI::(.*?) (.*?);\n(.*?)/package EAI::$1 $newVersion;\n$3/s if $doNewVersion;
 $data =~ s/Copyright \(c\) (\d{4}) Roland Kapl/Copyright \(c\) $curYear Roland Kapl/s;
 write_file("lib/EAI/Wrap.pm", $data);
 
 for my $libfile ("Common","File","FTP","DB","DateUtil") {
-	print "updating version for $libfile\n";
+	print "updating ".($doNewVersion ? "version to $newVersion/year of copyright" : "copyright")." for $libfile.pm\n";
 	my $data = read_file ("lib/EAI/$libfile.pm");
-	$data =~ s/^package EAI::(.*?) (.*?);\n(.*?)/package EAI::$1 $newVersion;\n$3/s if $newVersion;
+	$data =~ s/^package EAI::(.*?) (.*?);\n(.*?)/package EAI::$1 $newVersion;\n$3/s if $doNewVersion;
 	$data =~ s/Copyright \(c\) (\d{4}) Roland Kapl/Copyright \(c\) $curYear Roland Kapl/s;
 	write_file("lib/EAI/$libfile.pm", $data);
 }
-print "enter to finish.";
+print "updating copyright for LICENSE\n";
+$data = read_file ("LICENSE");
+$data =~ s/Copyright \(c\) 2023-(\d{4}) Roland Kapl/Copyright \(c\) 2023-$curYear Roland Kapl/s;
+write_file("LICENSE", $data);
+
+print "enter to finish, don't forget to add changes to Changes file before creating/uploading the distribution!";
 <STDIN>;
 
 sub read_file {
